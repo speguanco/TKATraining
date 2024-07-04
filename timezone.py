e5fef28b9d892090dda16e7f1c3e62bf33c5ab46
Spe <iamthewalrus050208@gmail.com>
2024-07-05 01:25:49 +0800

    adjusted

--- timezone.py
+++ timezone.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 import time
-#comment out code
+#code to print out clock
 curr_time = time.localtime
 curr_clock = time.strftime("%H:%M:%S", curr_time)
 
 print(curr_clock)
-=======
-import time
-/* code to print out clock */
-curr_time = time.localtime
-curr_clock = time.strftime("%H:%M:%S", curr_time)
-
-print(curr_clock)
->>>>>>> main
