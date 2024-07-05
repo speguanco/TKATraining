d5bee007e0b8334d27077a8a06946f83c0334012
Spe <iamthewalrus050208@gmail.com>
2024-07-05 09:06:08 +0800

    fixed python code for turtle for loop

--- timezone.py
+++ timezone.py
@@ -12,10 +12,10 @@ star = turtle.Turtle()
 star.right(75)
 star.forward(100)
 
-for i in range(4)
+for i in range(4):
     star.right(144)
     star.forward(100)
 
 turtle.done()
-=======
+
 print(curr_time)
