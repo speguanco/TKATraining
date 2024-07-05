ec776c10ef266b6d0a5a39a25135ef0ac305c1d7
jr-Garciatraining <pbbp3496@gmail.com>
2024-07-05 08:48:49 +0800

    Feat: Add turtle

--- timezone.py
+++ timezone.py
@@ -1,6 +1,18 @@
 import time
+import turtle
 #code to print out clock
 curr_time = time.localtime
 curr_clock = time.strftime("%H:%M:%S", curr_time)
 
 print(curr_clock)
+
+star = turtle.Turtle()
+
+star.right(75)
+star.forward(100)
+
+for i in range(4)
+    star.right(144)
+    star.forward(100)
+
+turtle.done()
